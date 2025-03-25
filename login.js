13f4468714dca736ed4374f161c8dc900df9a2c0
Disha D <disha16122001@gmail.com>
2025-03-25 09:31:48 +0530

    Initial commit

---
+++ login.js
@@ -0,0 +1,22 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let errorMessage = document.getElementById("errorMessage");
+    let forgotPasswordLink = document.getElementById("forgotPassword");
+
+    if (username !== "sapna.deepak" || password !== "Disha@16") {
+        errorMessage.textContent = "Incorrect username or password!";
+        forgotPasswordLink.style.display = "block";
+    } else {
+        errorMessage.textContent = "";
+        forgotPasswordLink.style.display = "none";
+        // Redirect to homepage.html
+        window.location.href = "homepage.html";
+    }
+});
+
+document.getElementById("forgotPassword").addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("Contact helpdesk to reset your password.");
+});
