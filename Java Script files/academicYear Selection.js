10f403e0a1ac754feab61dd5b306300499de0edf
Disha <disha16122001@gmail.com>
2026-03-27 23:11:25 +0530

    add 2026-27 file

--- Java Script files/academicYear Selection.js
+++ Java Script files/academicYear Selection.js
@@ -1,6 +1,8 @@
 function continueToDashboard() {
     const selectedYear = document.getElementById("academicYear").value;
-    if (selectedYear === "apr25-mar26") {
+    if (selectedYear === "apr26-mar27") {
+        window.location.href = "dashboard-26-27.html";
+    } else if (selectedYear === "apr25-mar26") {
         window.location.href = "dashboard-25-26.html";
     } else {
         window.location.href = "dashboard-otherYears.html";
